3f9480ee3309a0fa72b8afcdffe2c9376f19c5f5
clavo-sk <sophia.clavo@student.ateneo.edu>
2025-03-02 15:42:00 +0800

    Update models.py

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    ingredientName = models.CharField(max_length=100)
+
+class Recipe(models.Model):
+    recipeName = models.CharField(max_length=100)
+    
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=100)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
