06b970532017ac3a4fd49f91845cd19fd3500421
clavo-sk <sophia.clavo@student.ateneo.edu>
2025-03-03 23:07:09 +0800

    Update models.py
    updated code format

--- ledger/models.py
+++ ledger/models.py
@@ -23,5 +23,9 @@ class Recipe(models.Model):
     
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
-    ingredients = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name = "recipe")
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name = "ingredients")
+    ingredient = models.ForeignKey(Ingredient,
+                                   on_delete=models.CASCADE,
+                                   related_name = "recipe")
+    recipe = models.ForeignKey(Recipe, 
+                               on_delete=models.CASCADE, 
+                               related_name = "ingredients")
