328f9385bc86bf919453af5206e34263d81daab1
clavo-sk <sophia.clavo@student.ateneo.edu>
2025-03-03 23:12:26 +0800

    Update models.py

--- ledger/models.py
+++ ledger/models.py
@@ -12,6 +12,7 @@ class Ingredient(models.Model):
     def get_absolute_url(self):
         return reverse('ingredient_detail', args=[str(self.name)])
 
+
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
     
@@ -20,7 +21,8 @@ class Recipe(models.Model):
         
     def get_absolute_url(self):
         return reverse('ledger:recipe_detail', args=[str(self.pk)])
-    
+
+
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
     ingredient = models.ForeignKey(Ingredient,
